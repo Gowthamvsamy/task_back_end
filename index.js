481af36b7c1c4b513c52cf4da5508544604cafaa
Gowthamvsamy <gowtham.vsamy@gmail.com>
2025-03-12 14:39:28 +0530

    create json data file

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 // import
 const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 // create express API_server
 const API_SERVER = express();
 
+// Middleware
+API_SERVER.use(cors());
+API_SERVER.use(bodyParser.json());
+
+// File path for storing data
+const filePath = "./data.json";
+
 // Environment settings
 require('dotenv').config();
+
+// API to save a form data
+API_SERVER.post("/save-data", (req, res) => {
+    const newData = req.body;
+
+    fs.readFile(filePath, "utf8", (err, data) => {
+        let jsonData = [];
+
+        if(!err && data){
+            jsonData = JSON.parse(data)
+        }
+
+        jsonData.push(newData);
+
+        fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), (err) => {
+            if(err) {
+                return res.status(500).json({message : "Error writing file"});
+            }
+            res.status(200).json({message: "Data saved successfully"});
+        });
+    });
+});
 
 API_SERVER.listen(process.env.PORT, process.env.HOSTNAME, function () {
     console.log("server start")
